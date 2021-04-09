3f0371609bff8a7d480716b718810d5b3911dad0
rajktmrider <mrajcivil96@gmail.com>
2021-04-09 20:15:12 +0530

    crazypyscho

---
+++ src/test/java/org/lifeo/HellString.java
@@ -0,0 +1,14 @@
+package org.lifeo;
+
+public class HellString {
+	private void sample1() {
+		 System.out.println("sample1");
+		
+	}
+	 private void sample2() {
+		System.out.println("sample2");
+	}
+	 private void sample3() {
+		System.out.println("sample3");
+	}
+}
